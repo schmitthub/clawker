4bad7e1a00245943ae83c8ad98cc17571aab57d4
Andrew J Schmitt <8697299+schmitthub@users.noreply.github.com>
2026-01-16 15:13:30 -0800

    Refactor Start Command (#34)
    * moved container start to sub package. aliased top level start to sub package. created serena memory on error and output issues. added serena to vscode
    
    * readme was too bloated.

--- pkg/cmd/start/start.go
+++ pkg/cmd/start/start.go
@@ -2,7 +2,7 @@
 package start
 
 import (
-	"github.com/schmitthub/clawker/pkg/cmd/container"
+	"github.com/schmitthub/clawker/pkg/cmd/container/start"
 	"github.com/schmitthub/clawker/pkg/cmdutil"
 	"github.com/spf13/cobra"
 )
@@ -10,7 +10,7 @@ import (
 // NewCmdStart creates the start command as an alias to container start.
 // This follows Docker's pattern where `docker start` is an alias for `docker container start`.
 func NewCmdStart(f *cmdutil.Factory) *cobra.Command {
-	cmd := container.NewCmdStart(f)
+	cmd := start.NewCmdStart(f)
 	cmd.Use = "start CONTAINER [CONTAINER...]"
 	return cmd
 }
